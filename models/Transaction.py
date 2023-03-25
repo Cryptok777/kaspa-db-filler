d643b9aec9189cdc71990ab907ef29cc9edd77c4
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2023-03-25 03:25:44 -0400

    previous_outpoint_index to int

--- models/Transaction.py
+++ models/Transaction.py
@@ -40,13 +40,9 @@ class TransactionInput(Base):
     transaction_id = Column(String)
     index = Column(Integer)
 
-    previous_outpoint_hash = Column(
-        String
-    )  # "ebf6da83db96d312a107a2ced19a01823894c9d7072ed0d696a9a152fd81485e"
-    previous_outpoint_index = Column(
-        String
-    )  # "ebf6da83db96d312a107a2ced19a01823894c9d7072ed0d696a9a152fd81485e"
+    previous_outpoint_hash = Column(String)
+    previous_outpoint_index = Column(Integer)
 
-    signature_script = Column(String)  # "41c903159094....281a1d26f70b0037d600554e01",
+    signature_script = Column(String)
     sig_op_count = Column(Integer)
-    block_time = Column(BigInteger)  # "1663286480803"
+    block_time = Column(BigInteger)
